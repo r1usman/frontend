f6243e34d240d96e2f19407dfd8ade28160630cd
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-07 18:58:01 +0500

    Created

---
+++ src/AppCourses/Admin/Components/TagInput.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const TagInput = ({ tags, setTags }) => {
+  const [input, setInput] = useState("");
+  console.log(tags);
+  
+
+  const handleKeyDown = (e) => {
+    if ((e.key === "Enter" || e.key === ",") && input.trim()) {
+      e.preventDefault();
+      const newTag = input.trim();
+      if (!tags.includes(newTag)) {
+        setTags([...tags, newTag]);
+        setInput("");
+      }
+    } else if (e.key === "Backspace" && !input && tags.length) {
+      setTags(tags.slice(0,-1));
+    }s
+  };
+
+  const handleRemove = (index) => {
+    const newTags = tags.filter((_, i) => i !== index);
+    setTags(newTags);
+  };
+
+  return (
+    <div className="flex flex-wrap gap-2 items-center border border-gray-300 rounded-md px-2 py-2 min-h-[48px] mt-3">
+      {tags.map((tag, index) => (
+        <div
+          key={index}
+          className="flex items-center bg-sky-100/70 text-sky-700 px-3 py-1 rounded-full text-sm font-medium"
+        >
+          {tag}
+          <button
+            type="button"
+            className="ml-2 text-sky-500 hover:text-sky-700 font-bold cursor-pointer"
+            onClick={() => handleRemove(index)}
+          >
+            &times;
+          </button>
+        </div>
+      ))}
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+        placeholder="Type and press Enter"
+        className="flex-1 min-w-[120px] border-none outline-none text-sm p-1"
+      />
+    </div>
+  );
+};
+
+export default TagInput;
