83088823290ca8528479b732dfba8a3cb0dfa5a8
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-09 21:14:42 +0500

    Update

---
+++ src/AppCourses/Admin/Stats.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+
+const Stat = ({ totalLikes = 0, totalViews = 0 }) => {
+
+    return (
+        <div >
+            Stats
+        </div>
+    );
+};
+
+export default Stat;
