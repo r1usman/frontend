54021b814f7721d5a123fc22413dad54219e2883
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-09 21:12:41 +0500

    Update

--- src/AppCourses/Admin/Components/CourseCard.jsx
+++ src/AppCourses/Admin/Components/CourseCard.jsx
@@ -1,16 +1,31 @@
 import { Edit } from 'lucide-react';
 import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../../GlobalContext/UserContext';
+import { getLightColorFromImage } from '../../../Utility/Helper';
 
-const CourseCard = ({ imgurl, title, lastUpdated, onselect, DeletedArray ,dueDate, ID , tag}) => {  
+const CourseCard = ({at,data, imgurl, title, lastUpdated, onselect, DeletedArray ,dueDate, ID , tag}) => {  
     const {User} =useContext(UserContext)
+    const [bgColor, setBgColor] = useState("#F5F4FF");
+
+      useEffect(() => {
+    if (imgurl) {
+        getLightColorFromImage(imgurl)
+        .then((color) => {
+            setBgColor(color);
+        })
+        .catch(() => {
+            setBgColor("#ffffff");
+        });
+    }
+    }, [imgurl]);
+
     
     return(
         <div
             className={`${DeletedArray ? DeletedArray?.includes(ID)?"border border-purple-600 ":"" : ""} h-[300px] flex flex-col items-center justify-between bg-white rounded-lg border border-gray-200  relative cursor-pointer hover:border-purple-600 overflow-hidden transition`}
-            style={{backgroundColor: "#F5F4FF"}}
+            style={{backgroundColor: bgColor}}
 
-            onClick={tag == "Edit" ? onselect :()=>onselect(ID) }
+            onClick={()=>{tag == "Edit" ? onselect(data) : "" }}
             >
                 {
                 tag=="Performance"  && (
@@ -25,12 +40,12 @@ const CourseCard = ({ imgurl, title, lastUpdated, onselect, DeletedArray ,dueDat
                 )
             }
 
-            <div className="p-2">
+            <div className="p-2 ">
                 {imgurl ? (
                 <img
                     src={imgurl}
                     alt="Resume Thumbnail"
-                    className=" h-[220px] rounded"
+                    className={`${at=="Admin" ?"h-[190px] mt-3 " :"h-[220px] "}rounded`}
                 />
                 ) : (
                 <span className="text-gray-400">No Preview Available</span>
@@ -39,12 +54,20 @@ const CourseCard = ({ imgurl, title, lastUpdated, onselect, DeletedArray ,dueDat
             <div className='w-full bg-white px-4 py-3'>
                 <h3 className="text-sm font-medium truncate overflow-hidden whitespace-nowrap">{title}</h3>
                 {
-                    User.status == "Instructor" ?
-                    <p className="text-xs font-medium text-gray-500 mt-0.5">Last Update :  {lastUpdated}</p>
-                    :
-                    <p className="text-xs font-medium text-gray-500 mt-0.5">Due Date :  {dueDate}</p>
-
+                    User.status == "Instructor" && (
+                                            <p className="text-xs font-medium text-gray-500 mt-0.5">Last Update :  {lastUpdated}</p>
+                    )
                 }
+                {
+                    User.status == "Student" && (
+                                        <p className="text-xs font-medium text-gray-500 mt-0.5">Due Date :  {dueDate}</p>                    )
+                }
+                {
+                    User.status == "Admin" && (
+                                            <p className="text-xs font-medium text-gray-500 mt-0.5">Last Update :  {lastUpdated}</p>
+                    )
+                }
+                
             </div>
         </div>
     )
