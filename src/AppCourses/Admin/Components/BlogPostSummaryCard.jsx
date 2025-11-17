00d037efa6b764f014c768edf1ed85616118a542
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-17 20:44:49 +0500

    Update

--- src/AppCourses/Admin/Components/BlogPostSummaryCard.jsx
+++ src/AppCourses/Admin/Components/BlogPostSummaryCard.jsx
@@ -1,6 +1,16 @@
 import { LuEye, LuHeart, LuTrash2 } from "react-icons/lu";
 import python from "../../assests/python.jpg"
-const BlogPostSummaryCard = ({at , title, imgUrl, updatedOn, tags, views, likes, onClick, onDelete }) => {
+import { LucideView } from "lucide-react";
+import { useState } from "react";
+import BlogPostView from "../BlogPostView";
+import Modal from "../../../DashBoard/Modals/Modal";
+const BlogPostSummaryCard = ({at , title,content, imgUrl, updatedOn, tags, views, likes, onClick, onDelete }) => {
+    const [AllowViewBlog, setAllowViewBlog] = useState(false)
+    const handleView = (e)=>{
+        e.stopPropagation();
+        setAllowViewBlog(true)
+    }
+
     return (
         <div
             className="flex items-start gap-4 bg-gray-50 border border-gray-200  p-3 mb-3 rounded-lg cursor-pointer  group"
@@ -45,17 +55,36 @@ const BlogPostSummaryCard = ({at , title, imgUrl, updatedOn, tags, views, likes,
                 ))}
                 </div>
             </div>
+        
             </div>
+            <div className="flex flex-col gap-3">
+                <button
+                    className="flex items-center gap-2 text-xs font-medium text-sky-500 bg-sky-50/60 rounded px-1.5 md:px-3 py-1.5 md:py-[3px] border border-sky-100 hover:border-sky-400 cursor-pointer hover:scale-[1.02] transition-all   "
+                    onClick={handleView}
+                    >
+                    <LuEye className="text-sm" />
+                    <span>View Blog</span>
+            
+                </button>
 
-            <button
-            className="hidden md:flex items-center gap-2 text-xs text-rose-500  font-medium bg-rose-50 px-3 py-1 rounded text-nowrap border border-rose-100  hover:border-rose-200 cursor-pointer"
-            onClick={(e) => {
-                e.stopPropagation();
-                onDelete();
-            }}
+                    <button
+                    className="hidden md:flex items-center gap-2 text-xs text-rose-500  font-medium bg-rose-50 px-3 py-1 rounded text-nowrap border border-rose-100  hover:border-rose-200 cursor-pointer"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        onDelete();
+                    }}
+                    >
+                    <LuTrash2 /> <span className="hidden md:block">Delete</span>
+                    </button>
+            </div>
+             <Modal
+                onClose={(e) => {setAllowViewBlog(false) , e.stopPropagation();  }}
+                isOpen={AllowViewBlog}
+                type="BlogView"
+                title={`${content?.title}`}
             >
-            <LuTrash2 /> <span className="hidden md:block">Delete</span>
-            </button>
+               <BlogPostView Blog ={content}/>
+        </Modal>
         </div>
     );
 
