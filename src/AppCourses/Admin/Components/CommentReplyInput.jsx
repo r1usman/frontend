828fc635a878645874d2ef85aadac3d7e903ac70
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-19 21:20:12 +0500

    Update

--- src/AppCourses/Admin/Components/CommentReplyInput.jsx
+++ src/AppCourses/Admin/Components/CommentReplyInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LuSend } from "react-icons/lu";
 
 const CommentReplyInput = ({
     user,
@@ -12,38 +13,43 @@ const CommentReplyInput = ({
     type,
 }) => {
     return (
-    <div className="bg-gray-50 p-3 rounded-lg mt-3">
+    <div className="bg-gray-50 p-4 rounded-lg  bg-white">
         <div className="flex items-start gap-3">
         <img
             src={user?.profileImage || "/default-avatar.png"}
             alt={authorName}
             className="w-8 h-8 rounded-full object-cover"
         />
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col gap-1">
+            <h1 className="text-xs text-gray-500 font-medium">{authorName}</h1>
             <textarea
             value={replyText}
             onChange={(e) => setReplyText(e.target.value)}
             placeholder={disableAutoGen ? "Write your comment..." : "Type your reply..."}
-            className="w-full border border-gray-300 rounded-md p-2 text-sm resize-none focus:outline-none focus:ring-1 focus:ring-sky-400"
+            className="form-input-Course resize-none"
             rows={3}
             />
 
 
-            <div className="flex items-center gap-2 mt-2">
-            <button
-                onClick={handleAddReply}
-                disabled={!replyText.trim()}
-                className="bg-sky-500 text-white px-3 py-1 rounded hover:bg-sky-600 disabled:opacity-50"
-            >
-                {type === "new" ? "Add Comment" : "Reply"}
-            </button>
+            <div className="flex items-center justify-end    gap-2 mt-2">
 
             <button
                 onClick={handleCancelReply}
-                className="bg-gray-200 text-gray-700 px-3 py-1 rounded hover:bg-gray-300"
+                className="bg-gray-200 cursor-pointer rounded-2xl font-medium text-gray-700 px-3 py-1 rounded hover:bg-gray-300"
             >
                 Cancel
             </button>
+            <button
+                onClick={handleAddReply}
+                disabled={!replyText.trim()}
+                className="flex rounded-2xl items-center gap-2 text-sm font-medium text-sky-600 bg-sky-100 rounded px-3 py-1 border border-sky-200 hover:border-sky-400 cursor-pointer hover:scale-105 transition-all"
+            >
+                <LuSend />
+                <span>{type === "new" ? "Add Comment" : "Reply"}</span>
+            </button>
+
+
+            
             </div>
         </div>
         </div>
