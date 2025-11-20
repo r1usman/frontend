07a12065b04e66f364a4fbffa1cb9716a0cd1f12
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-20 20:06:38 +0500

    Update

--- src/AppCourses/Admin/Stats.jsx
+++ src/AppCourses/Admin/Stats.jsx
@@ -1,11 +1,38 @@
 import React from "react";
-import { LineChart } from "@mui/x-charts/LineChart";
 
-const Stat = ({ totalLikes = 0, totalViews = 0 }) => {
+import DashboardSummaryCard from "./Components/DashboardSummaryCard";
+import { LuArchive, LuChartLine, LuCheckCheck, LuHeart } from "react-icons/lu";
+import { Archive } from "lucide-react";
+
+const Stat = ({ totalLikes = 0, totalViews = 0  , numberofblogs}) => {
 
     return (
-        <div >
-            Stats
+        <div className="flex flex-col gap-3">
+            <DashboardSummaryCard
+                icon={<LuChartLine />}
+                label="Views"
+                value={totalViews || 0}
+                bgColor="bg-sky-100/60"
+                color="text-sky-500"
+                />
+
+            <DashboardSummaryCard
+                icon={<LuHeart />}
+                label="Likes"
+                value={totalLikes || 0}
+                bgColor="bg-sky-100/60"
+                color="text-sky-500"
+                />
+            <DashboardSummaryCard
+
+                icon={<LuArchive />}
+                label="Blogs"
+                value={numberofblogs || 0}
+                bgColor="bg-sky-100/60"
+                color="text-sky-500"
+            />
+            
+
         </div>
     );
 };
