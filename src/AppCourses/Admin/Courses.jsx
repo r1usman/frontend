780ee1d55395bd51aff032e14b599723eb7eec04
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-20 20:06:09 +0500

    Update

--- src/AppCourses/Admin/Courses.jsx
+++ src/AppCourses/Admin/Courses.jsx
@@ -44,13 +44,13 @@ const Courses = () => {
     <div className="relative">
       <div className="h-[110vh] font-urbanist grid grid-cols-1 md:grid-cols-3 md:gap-3 pt-1 pb-6 px-4 md:px-0 min-h-screen ">
         <div
-          className="h-[300px] flex flex-col gap-5 items-center justify-center border-2 border-dashed border-purple-300 rounded-md cursor-pointer"
+          className="h-[300px] flex flex-col gap-5 items-center justify-center border-2 border-dashed border-sky-500 rounded-md cursor-pointer"
           onClick={() => setOpenCreateModal(true)}
         >
-          <div className="w-12 h-12 flex items-center justify-center bg-purple-200/60 rounded-full">
-            <LucideCirclePlus className="text-xl text-purple-500" />
+          <div className="w-12 h-12 flex items-center justify-center bg-sky-200/60 rounded-full">
+            <LucideCirclePlus className="text-xl text-sky-500" />
           </div>
-          <h3 className="font-medium text-gray-800">Add New Assingment</h3>
+          <h3 className="font-medium text-gray-800">Add New Course</h3>
         </div>
 
         {allCourses?.map((challenge) => (
