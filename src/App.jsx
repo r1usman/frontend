25aa0575c1cdad87ed57cffbb6db919e94d240fd
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-23 21:18:48 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ import BlogPost from "./AppCourses/Admin/BlogPost.jsx";
 import Courses from "./AppCourses/Admin/Courses.jsx";
 import EditBlog from "./AppCourses/Admin/EditBlog.jsx";
 import ShowDefaultCourses from "./AppCourses/Student/ShowDefaultCourses.jsx";
+import Comments from "./AppCourses/Admin/Components/Comments .jsx";
 
 
 function App() {
@@ -117,6 +118,7 @@ function App() {
             <Route path="Courses" element={<Courses />} />
             <Route path="CreateBlog" element={<EditBlog />} />
             <Route path="edit/:postSlug" element={<EditBlog  isEdit={true}/>} />
+            <Route path="Commemt" element={<Comments />} />
           </Route>
 
 
